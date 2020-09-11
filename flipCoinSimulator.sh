6436d87a7f2571c4adfb900a4af2aa6a192f629e
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-11 15:22:44 +0530

    UC4: Game Continues after Tie upto minimum difference is 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,15 +3,9 @@ read -p "Enter number of times loop to be executed:" n
 read -p "Enter number of times after which loop should break:" a
 Head=0
 Tail=0
-if [ $n -lt $a ]
-then
-echo "error:loop times should greater than loop breaking condition"
-fi
-if [ $n -ge $a ]
-then
-for (( i=0; i<=n; i++ ))
-do
-     flip=$(( RANDOM%2 ))
+tie=0
+function fun1(){
+flip=$(( RANDOM%2 ))
 
            if [ $flip -eq "1" ]
            then
@@ -20,28 +14,57 @@ do
            else
                ((Tail++))
            fi
-            if [[ $Head -eq $a || $Tail -eq $a ]]
+}
+if [ $n -lt $a ]
+then
+echo "error:loop times should greater than loop breaking condition"
+fi
+
+function fun2(){
+
+for (( i=0; i<=n; i++ ))
+do
+           fun1
+           if [[ $Head -eq $a || $Tail -eq $a ]]
                  then
+                      echo "Head wins ="$Head
+                      echo "Tail wins ="$Tail
+
+                       if [ $Head -gt $Tail ]
+                       then
+                              c=$(( $Head-$Tail ))
+                          echo "Head wins by $c times"
+
+                          break
+                       fi
+                       if [ $Tail -gt $Head ]
+                         then
+                              d=$(( $Tail-$Head ))
+                              echo "Tail wins by $d times"
+
                        break
+                       fi
+                       if [ $Head -eq $Tail ]
+                          then
+                                 c=$(( $Head-$Tail ))
+                                 d=$(( $Tail-$Head ))
+                                   if [[ $c != 2 || ! $d != 2 ]]
+                                then
+                                    fun2
+
+                        fi
+
+
                  fi
+
+fi
 
 done
+}
+#### main script
+if [ $n -ge $a ]
+then
+ fun1
+    fun2
 fi
- echo "Head wins ="$Head
- echo "Tail wins ="$Tail
-         if [ $Head -gt $Tail ]
-                  then
-                       won1=$(( $Head-$Tail ))
-                       echo "Head won by $won1 times"
-         fi
 
-                 if [ $Tail -gt $Head ]
-                 then
-                        won2=$(( $Tail-$Head )) 
-
-                        echo "Tail won by $won2 times"
-                  fi
-                 if [ $Head -eq $Tail ]
-                     then
-                      echo " Match ties "
-                 fi
