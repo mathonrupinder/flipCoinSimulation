05a7eaee7323a3323b18e5756bac918fd07ec829
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-11 13:33:12 +0530

    UC3: Modified UC2 till Head or Tail won 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,7 +1,14 @@
 echo "Welcome to FLIP COIN Simulalation problem Solution"
-read -p "Enter number of loop to be executed:" n
+read -p "Enter number of times loop to be executed:" n
+read -p "Enter number of times after which loop should break:" a
 Head=0
 Tail=0
+if [ $n -lt $a ]
+then
+echo "error:loop times should greater than loop breaking condition"
+fi
+if [ $n -ge $a ]
+then
 for (( i=0; i<=n; i++ ))
 do
      flip=$(( RANDOM%2 ))
@@ -13,8 +20,28 @@ do
            else
                ((Tail++))
            fi
+            if [[ $Head -eq $a || $Tail -eq $a ]]
+                 then
+                       break
+                 fi
 
 done
+fi
  echo "Head wins ="$Head
  echo "Tail wins ="$Tail
+         if [ $Head -gt $Tail ]
+                  then
+                       won1=$(( $Head-$Tail ))
+                       echo "Head won by $won1 times"
+         fi
 
+                 if [ $Tail -gt $Head ]
+                 then
+                        won2=$(( $Tail-$Head )) 
+
+                        echo "Tail won by $won2 times"
+                  fi
+                 if [ $Head -eq $Tail ]
+                     then
+                      echo " Match ties "
+                 fi
