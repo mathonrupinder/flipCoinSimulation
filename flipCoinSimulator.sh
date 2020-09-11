e67de8c136b92924e760b1e0aa73b18b16d6b987
mathonrupinder <mathonrupinder97@gmail.com>
2020-09-11 13:12:36 +0530

    UC2: Flip a Coin Multiple Times using Loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,20 @@
 echo "Welcome to FLIP COIN Simulalation problem Solution"
+read -p "Enter number of loop to be executed:" n
 Head=0
 Tail=0
-flip=$(( RANDOM%2 ))
-if [ $flip -eq "1" ]
-then
-    echo "head wins"
-else
-    echo "tail wins"
-fi
+for (( i=0; i<=n; i++ ))
+do
+     flip=$(( RANDOM%2 ))
 
+           if [ $flip -eq "1" ]
+           then
+               ((Head++))
+
+           else
+               ((Tail++))
+           fi
+
+done
+ echo "Head wins ="$Head
+ echo "Tail wins ="$Tail
 
